6e524e07c6ac2b84b892440fd4b204b6b10eae7f
zdc4be <zdcalhoun94@gmail.com>
2016-09-14 10:39:35 -0400

    Update project1.R

--- project1.R
+++ project1.R
@@ -6,10 +6,12 @@ path <- "D:/Users/Bae/Documents/School/Fall 2016/SYS 4021/Data/Train Data"
 setwd("")
 source("")
 path <- ""
-#Zach Calhoun
-setwd("")
-source("")
-path <- ""
+#Zach Calhoun -- I sourced everything
+setwd("C:/Users/Student/Desktop/Fall 2016/SYS 4021/Univariate")
+source("AccidentInput.R")
+source("PCAplots.R")
+source("SPM_Panel.R")
+path <- "C:/Users/Student/Desktop/Fall 2016/SYS 4021/Univariate/path"
 
 #Run all code
 #acts is the array of all the years separately
@@ -18,3 +20,22 @@ comvar <- intersect(colnames(acts[[1]]), colnames(acts[[8]]))
 #totacts is the array of all data
 totacts <- combine.data(acts, comvar)
 dim(totacts)
+
+#############################
+#ZC
+#   Remove duplicates to clean data
+#
+#############################
+
+dim(totacts)
+totactsnd <- totacts[!(duplicated(totacts[, c("INCDTNO", "YEAR", "MONTH", "DAY", "TIMEHR", "TIMEMIN")])),]
+dim(totactsnd)
+
+#############################
+#ZC
+#   Set up extreme points (in case we want to use)
+#
+#############################
+
+dmgbox <- boxplot(totacts$ACCDMG)
+xdmg <- totacts[totacts$ACCDMG > dmgbox$stats[5],]
